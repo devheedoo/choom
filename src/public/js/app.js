02ed0900dcd8e6eebebaf87ccf764f6bc4f906c3
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-30 23:14:20 +0900

    feat(client): Send enter_room event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,12 @@
 const socket = io(); // from socket.io
+
+const formRoom = document.querySelector('#roomForm');
+const inputRoomId = formRoom.querySelector('input');
+
+formRoom.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('enter_room', { roomId: inputRoomId.value }, () =>
+    console.log('enter_room server response is done')
+  );
+  inputRoomId.value = '';
+});
