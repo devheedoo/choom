8d1418ea910014075f4314b2a956ab9db91fc934
Choco Kim <devheedoo@gmail.com>
2021-12-31 04:18:33 +0900

    refactor: Rename currentRoomName > currentRoomId

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,19 +9,19 @@ const h3Room = divRoom.querySelector('h3');
 const ulRoom = divRoom.querySelector('ul');
 const formMessage = divRoom.querySelector('#formMessage');
 const inputMessage = formMessage.querySelector('input');
-let currentRoomName = 'no_room_name';
+let currentRoomId = 'no_room_name';
 
 formRoom.addEventListener('submit', (e) => {
   e.preventDefault();
   socket.emit('join_room', { roomId: inputRoomId.value }, handleJoinRoom);
-  currentRoomName = inputRoomId.value;
+  currentRoomId = inputRoomId.value;
   inputRoomId.value = '';
 });
 
 function handleJoinRoom() {
   divWelcome.hidden = true;
   divRoom.hidden = false;
-  h3Room.innerHTML = currentRoomName;
+  h3Room.innerHTML = currentRoomId;
 }
 
 function addMessageToUlRoom(message) {
