f4c9b59051c930bcff5188d305a8081ffc35c45d
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-30 23:03:30 +0900

    feat(server): Create socket.io server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import WebSocket from 'ws';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -11,35 +11,10 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
-const websocketServer = new WebSocket.Server({ server: httpServer });
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => console.log(socket));
 
 httpServer.listen(3000, () =>
   console.log('Listening on http://localhost:3000 and ws://localhost:3000')
 );
-
-const websockets = [];
-
-websocketServer.on('connection', (websocket) => {
-  websockets.push(websocket);
-  websocket['nickname'] = 'Anonymous';
-  websocket.send('I AM THE SERVER');
-  websocket.on('message', (message) => {
-    // mesage: { type: 'nickname' | 'message'; payload: message }
-    const { type, payload } = JSON.parse(message);
-    switch (type) {
-      case 'nickname': {
-        websocket['nickname'] = payload;
-        break;
-      }
-      case 'message': {
-        websockets.forEach((ws) => {
-          ws.send(`${websocket.nickname}: ${payload}`);
-        });
-        break;
-      }
-    }
-  });
-  websocket.on('close', () => {
-    console.log('🚨 client disconnected');
-  });
-});
