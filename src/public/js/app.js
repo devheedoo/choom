fdc68d13e6720bed1ded2e5124ac1e91241b1833
Choco Kim <devheedoo@gmail.com>
2021-12-31 03:35:44 +0900

    feat(client): Handle room_enter event
    - Hide room form
    - Show room id and message form

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,12 +1,24 @@
 const socket = io(); // from socket.io
 
-const formRoom = document.querySelector('#formRoom');
+const divWelcome = document.querySelector('#welcome');
+const formRoom = divWelcome.querySelector('#formRoom');
 const inputRoomId = formRoom.querySelector('input');
+
+const divRoom = document.querySelector('#room');
+const h3Room = divRoom.querySelector('h3');
+const formMessage = divRoom.querySelector('#formMessage');
+const inputMessage = formMessage.querySelector('input');
+let currentRoomName = 'no_room_name';
 
 formRoom.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('enter_room', { roomId: inputRoomId.value }, () =>
-    console.log('enter_room server response is done')
-  );
+  socket.emit('enter_room', { roomId: inputRoomId.value }, handleEnterRoom);
+  currentRoomName = inputRoomId.value;
   inputRoomId.value = '';
 });
+
+function handleEnterRoom() {
+  divWelcome.hidden = true;
+  divRoom.hidden = false;
+  h3Room.innerHTML = currentRoomName;
+}
