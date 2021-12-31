fe445593e1e2b1e48217e192a3a41484ab3b0018
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2022-01-01 00:58:31 +0900

    feat(app): getMyStream

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
 const socket = io(); // from socket.io
+
+const divMyStream = document.getElementById('divMyStream');
+const videoMyStream = divMyStream.querySelector('video');
+
+async function getMyStream() {
+  try {
+    const myStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    videoMyStream.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMyStream();
